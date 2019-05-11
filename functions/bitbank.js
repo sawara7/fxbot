51d68ec8e4ea11571753916de400c8a89f4ef034
sawara <sawara7@gmail.com>
2019-05-12 00:14:26 +0900

    Add bitbank function.

---
+++ functions/bitbank.js
@@ -0,0 +1,44 @@
+const bitbank = require('node-bitbankcc');
+const env = require('./_env');
+
+const conf_public = {
+    'endPoint': env.URL_BB_PUBLIC,    // required
+    'keepAlive': false,               // optional, default false
+    'timeout': 3000,                  // optional, default 3000
+};
+
+const conf_private = {
+    'endPoint': env.URL_BB_PRIVATE,  	// required
+    'apiKey': env.ACCOUNT_BB,		    // required
+    'apiSecret': env.APIKEY_BB,		// required
+    'keepAlive': false,				// optional, default->false
+    'timeout': 3000					// optional, default->3000
+};
+
+const publicApi =new bitbank.PublicApi(conf_public);
+const privateApi = new bitbank.PrivateApi(conf_private);
+
+const Ticker = async (pair) => { 
+    const params = {'pair': pair};
+    const res = await publicApi.getTicker(params);
+    console.log(res.data);
+};
+
+const BuyMarket = async (pair, amount) => {
+    const params = {
+        'pair': pair,
+        'amount': amount,
+        'side': 'buy',
+        'type': 'market'
+    };
+    const result = await privateApi.postOrder(params);
+    console.log(result.data);
+};
+
+const DobitbankEarlyMorningFunction = async() => {
+    await BuyMarket('btc_jpy', 0.1);
+    await BuyMarket('mona_jpy', 500);
+    return 'ok';
+}
+
+exports.DobitbankEarlyMorningFunction = DobitbankEarlyMorningFunction;
---
+++ functions/oanda.js
@@ -0,0 +1,90 @@
+const env = require('./_env.js');
+const oanda = require("@oanda/v20/context");
+const ct = new oanda.Context(env.uri, 443, true, '');
+ct.setToken(env.apiToken);
+
+const TARGET_INSTRUMENT = 'USD_JPY';
+const TARGET_UNIT = 1;
+const MAX_TRADE_COUNT = 500;
+
+const ORDER_REQUEST_1 = {
+    'instrument': TARGET_INSTRUMENT,
+    'units': TARGET_UNIT
+};
+
+const TRADE_REQUEST = {
+    'state': 'OPEN',
+    'instrument': TARGET_INSTRUMENT,
+    'count': MAX_TRADE_COUNT
+};
+
+let DoOrderMarket = async (id, orderRequest)=>{
+    let result;
+    ct.order.market(
+        id,
+        {
+            'instrument': 'USD_JPY',
+            'units': 10
+        },
+        (response) => {
+            result = response;
+        }
+    )
+
+    await (async () => {
+        while (true) {
+            if (result !== undefined) {
+                return;
+            }
+        }
+    })
+
+    return result;
+};
+
+let GetTrades = async (id, tradeRequest) => {
+    let result;
+    ct.trade.list(
+        id,
+        tradeRequest,
+        (response) => {
+            result = response.body.trades;
+        });
+
+    await (async () => {
+        while (true) {
+            if (result !== undefined) {
+                return;
+            }
+        }
+    })
+    
+    return result;
+};
+
+let DoTradesLoop = async (id, tradeRequest, tradeHundler) => {
+    let list = await GetTrades(id, tradeRequest);
+    for (let i in list) {
+        tradeHundler(list[i]);
+    }
+    return true;
+};
+
+let CloseOldestPosition = async (id, tradeRequest) => {
+    let list = await GetTrades(id, tradeRequest);
+    if (list.length === MAX_TRADE_COUNT) {
+        ct.trade.close(
+            id,
+            list[list.length-1].id,
+            {},
+            (response) => {
+                // Do Nothing
+            });
+    }
+    return true; // TODO: return response.
+};
+
+
+exports.DoOrderMarket = DoOrderMarket;
+exports.DoTradesLoop = DoTradesLoop;
+exports.CloseOldestPosition = CloseOldestPosition;

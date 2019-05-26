7f732d10141677f98237cbf0696f0ddc64f21dbf
tarax86 <sawara7@gmail.com>
2019-05-26 11:51:34 +0900

    Merge

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,6 @@ exports.bitbankAM4Function = functions.pubsub.schedule("00 12 * * *").onRun((con
     bb.DobitbankAM4Function();
 });
 
-<<<<<<< HEAD
 exports.bitbankAM7Function = functions.pubsub.schedule("00 15 * * *").onRun((context) => {
     bb.DobitbankAM7Function();
 });

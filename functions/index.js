cb304904ba3b730ecc8ef86e6dcf7f1787e95fd8
tarax86 <sawara7@gmail.com>
2019-05-26 11:51:10 +0900

    Merge

--- functions/index.js
+++ functions/index.js
@@ -11,8 +11,6 @@ exports.bitbankAM7Function = functions.pubsub.schedule("00 15 * * *").onRun((con
     bb.DobitbankAM7Function();
 });
 
-=======
->>>>>>> 276f4a428e1c7a0ecca41af3308c30bdec16c767
 exports.oandaFunction = functions.pubsub.schedule("59 13 * * *").onRun((context) => {
     oanda.OrderMarket();
 });
